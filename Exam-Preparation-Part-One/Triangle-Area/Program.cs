51a652046b5f7929848fe02c6653ec2c54b8ba01
Atanas Delyanov <atanas_delyanov@abv.bg>
2018-12-27 15:24:53 +0200

    chapter 8.1

---
+++ Exam-Preparation-Part-One/Triangle-Area/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Task 1
+
+namespace Triangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter x1: ");
+            int x1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter y1: ");
+            int y1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter x2: ");
+            int x2 = int.Parse(Console.ReadLine());
+            Console.Write("Enter y2: ");
+            int y2 = int.Parse(Console.ReadLine());
+            Console.Write("Enter x3: ");
+            int x3 = int.Parse(Console.ReadLine());
+            Console.Write("Enter y3: ");
+            int y3 = int.Parse(Console.ReadLine());
+
+            int a = Math.Abs(x2 - x3);
+            int h = Math.Abs(y3 - y1);
+            double s = a * (h / 2.0);
+            Console.WriteLine("The area is: " + s);
+        }
+    }
+}
