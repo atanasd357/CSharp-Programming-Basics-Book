e6198c91c886f7b174b56da4352a3bac9588b7be
Atanas Delyanov <atanas_delyanov@abv.bg>
2018-12-27 15:25:40 +0200

    chapter 8.2

---
+++ Exam-Preparation-Part-Two/Change-Tiles/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Task 2
+
+namespace Change_Tiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter budget: ");
+            decimal money = decimal.Parse(Console.ReadLine());
+            Console.Write("Enter floor width: ");
+            decimal floorWidth = decimal.Parse(Console.ReadLine());
+            Console.Write("Enter floor height: ");
+            decimal floorHeight = decimal.Parse(Console.ReadLine());
+            Console.Write("Enter triangle side: ");
+            decimal triangleSide = decimal.Parse(Console.ReadLine());
+            Console.Write("Enter triangle height: ");
+            decimal triangleHeight = decimal.Parse(Console.ReadLine());
+            Console.Write("Enter title price: ");
+            decimal tilePrice = decimal.Parse(Console.ReadLine());
+            Console.Write("Enter master price: ");
+            decimal masterPrice = decimal.Parse(Console.ReadLine());
+
+            decimal floorSquare = floorHeight * floorWidth;
+            decimal tileSquare = triangleSide * (triangleHeight / 2);
+            decimal tilesNeeded = Math.Ceiling(floorSquare/tileSquare) + 5;
+            decimal moneyNeeded = (tilePrice * tilesNeeded) + masterPrice;
+
+            if (moneyNeeded <= money)
+            {
+                Console.WriteLine("{0:f2} lv left.", money - moneyNeeded);
+            }
+            else if (moneyNeeded > money) 
+            {
+                Console.WriteLine("You'll need {0:f2} lv more.", moneyNeeded - money);
+            }
+        }
+    }
+}
