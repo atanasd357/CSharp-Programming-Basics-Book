ace6c7151eed1f8fb627861a9486e01302abc452
Atanas Delyanov <atanas_delyanov@abv.bg>
2018-12-27 15:20:34 +0200

    chapter 6

---
+++ Drawing-on-the-Console/House/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Task 9
+
+namespace House
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int stars = 1;
+            if (n % 2 == 0)
+            {
+                stars++;
+            }
+            int roofLength = (int)Math.Ceiling(n / 2f);
+
+            for (int i = 0; i < roofLength; i++)
+            {
+                int padding = (n - stars) / 2;
+                string line = new string('-', padding)
+                + new string('*', stars)
+                + new string('-', padding);
+                Console.WriteLine(line);
+                stars += 2;
+            }
+
+            for (int i = 0; i < n/2; i++)
+            {
+                string line = "|" + new string('*', n - 2) + "|";
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
---
+++ Drawing-on-the-Console/Rhombous-of-Stars/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Task 6
+
+namespace Rhombous_of_Stars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int row = 1; row <= n; row++)
+            {
+                for (int col = 1; col <= n-row; col++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("*");
+                for (int col = 1; col < row; col++)
+                {
+                    Console.Write(" *");
+                }
+                Console.WriteLine();
+            }
+
+            for (int row = 1; row < n; row++)
+            {
+                for (int col = 1; col <= row; col++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("*");
+                for (int col = 1; col < n-row; col++)
+                {
+                    Console.Write(" *");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
